15df70fc4cae10b06b24fe9967f7f9f20e31ef2c
Dimitry Ermakov <ermakovd06@gmail.com>
2025-07-21 15:21:22 -0400

    Update finetune_distributed.sh

--- finetune_distributed.sh
+++ finetune_distributed.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 
-### multi gpu ###
-### ***NOTE***: if set --main_process_port=0 (according to accelerate documentation, it will automatically choose a port number. but it seems not well implemented in deepspeed, if set 0, process hangs). so we need to specify a port number manually.
-TORCH_DISTRIBUTED_DEBUG=DETAIL ACCELERATE_DEBUG_VERBOSITY="debug" CUDA_VISIBLE_DEVICES="4,5,6,7" accelerate launch --main_process_port=29919 --mixed_precision=bf16 --dynamo_backend=no --num_machines=1 --num_processes=4 --use_deepspeed finetune_distributed.py 
+# Set environment variables for debugging (optional)
+export TORCH_DISTRIBUTED_DEBUG=DETAIL
+export ACCELERATE_DEBUG_VERBOSITY="debug"
 
-### single gpu ###
-# CUDA_VISIBLE_DEVICES="4" accelerate launch --main_process_port=29919 --mixed_precision=bf16 --dynamo_backend=no --num_machines=1 --num_processes=1 --use_deepspeed finetune_distributed.py 
+# Set CUDA devices - adjust based on your available GPUs
+export CUDA_VISIBLE_DEVICES="0,1,2,3"  # Use 4 GPUs for 7B model
+
+# Launch the distributed training
+accelerate launch \
+    --main_process_port=29919 \
+    --mixed_precision=bf16 \
+    --dynamo_backend=no \
+    --num_machines=1 \
+    --num_processes=4 \
+    --use_deepspeed \
+    finetune_distributed.py
